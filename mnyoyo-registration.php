0a38497fb48d72973271e78d18a5c9d172b05439
Justin Foell <justin@9seeds.com>
2013-02-17 17:00:04 -0600

    2012 edition commit

---
+++ mnyoyo-registration.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Plugin Name: MN Yo-Yo Registration Add-On
+Description: Registration customization for MN Yo-Yo (Uses Gravity Forms with PayPal)
+Version: 1.0
+Author: justin@foell.org
+*/
+
+/**
+ * Ladder has cost, except for Juniors
+ * 1A + Open = same cost as either
+ * Juniors can do 1A + Open for cost, or Ladder + Jr. Freestyle for free
+ */
+
+class MNYoYo_Registration {
+
+	public function onInit() {
+		//style sheet to hide certain elements
+		wp_register_style( 'mnyoyo', plugins_url( 'mnyoyo.css', __FILE__ ) );
+		wp_enqueue_style( 'mnyoyo' );
+		
+		//gravity forms custom dropdowns & save
+		add_filter( 'gform_pre_render', array( $this, 'filterGFormPreRender' ) );
+		add_filter( 'gform_validation', array( $this, 'filterGFormValidation' ) );
+	}
+
+	public function filterGFormPreRender( $form ) {		
+		if( $form['cssClass'] == 'mnyoyo-registration' )
+		{
+			//die('<pre>' . print_r($form, true));
+			
+			//queue the custom form mods
+			wp_register_script( 'mnyoyo', plugins_url( 'mnyoyo.js', __FILE__) , array( 'jquery' ) );
+			add_action( 'wp_footer', array( $this, 'onFooter' ) );
+			
+			//relies on parameter name being set
+			foreach( $form['fields'] as &$field ) {
+				//custom age list
+				if( $field['inputName'] == 'mnyoyo_age' ) {
+					//Creating drop down item array, with a blank default
+					$items = array( array( 'text' => 'Select Age', 'value' => '0' ) );
+					for ( $i = 1; $i < 100; $i++ ) {
+						$items[] = array( 'text' => $i,
+										  'value' => $i );
+					}
+				
+					$field['type'] = 'select';
+					$field['choices'] = $items;				
+				}
+			}
+		}
+ 
+		return $form;
+	}
+
+	public function filterGFormValidation( $data ) {
+		if ( $data['is_valid'] )
+			return $data;
+
+		$others_failed = false;
+		foreach ( $data['form']['fields'] as &$field ) {
+			if( $field['inputName'] == 'mnyoyo_age' &&
+			    $_REQUEST['input_' . $field['id']] == '0' ) {
+					$field['failed_validation'] = true;
+					$field['validation_message'] = 'This field is required.';
+					$others_failed = true;
+			} else if( $field['cssClass'] == 'mnyoyo-options' ) {
+				//hack to bypass validation due to checksum violations from changing the pricing
+				if( $field['validation_message'] == 'Invalid selection. Please select one of the available choices.' ) {
+					$field['failed_validation'] = false;
+					unset($field['validation_message']);
+				} else {
+					$others_failed = true;
+				}
+			} else {
+				if( $field['failed_validation'] )
+					$others_failed = true;
+			}			
+		}
+		if ( !$others_failed )
+			$data['is_valid'] = true;
+		//file_put_contents('/tmp/form.txt', print_r($_REQUEST, true) . print_r($data, true));
+		return $data;
+	}
+
+	public function onFooter() {
+		wp_print_scripts( 'mnyoyo' );
+	}
+}
+
+$mnyoyo = new MNYoYo_Registration();
+add_action( 'init', array( $mnyoyo, 'onInit' ) );
---
+++ mnyoyo.js
@@ -0,0 +1,202 @@
+var mnYoYo;
+
+(function($) {
+
+mnYoYo = {
+
+	//customize these for your form
+	ageCutoff : 14,
+	idPrefix : 'choice_3_',
+	idLadder : 2,
+	id1A : 3,
+	idOpen : 4,
+	idJrFreestyle : 5,
+
+	//don't touch anything below here
+	ladderCost : 0,
+	oneACost : 0,
+	openCost : 0,
+	ageId : '.mnyoyo-age select',
+	oneAId : '',
+	openId : '',
+	jrId : '',
+	ladderId : '',
+
+	init : function() {
+		//skip everything on the thank you 'page'
+		if( $('[id^="gform_submit_button_"]') ) {
+			this.setupVariables();
+			this.attachListeners();
+
+			//set up some stuff incase the form was invalidated
+			this.onAgeChange($(this.ageId));
+			this.onUpperDivisionChange( $(this.oneAId) );
+			this.onUpperDivisionChange( $(this.openId) );
+		}
+	},
+
+	setupVariables : function () {
+		this.oneAId = this.getJqId( this.id1A );
+		this.openId = this.getJqId( this.idOpen );
+		this.jrId = this.getJqId( this.idJrFreestyle );
+		this.ladderId = this.getJqId( this.idLadder );
+
+		this.oneACost = this.getCost(this.oneAId);
+		this.openCost = this.getCost(this.openId);
+		this.ladderCost = this.getCost(this.ladderId);
+	},
+
+	getJqId : function( id ) {
+		return '#' + this.idPrefix + id;
+	},
+
+	attachListeners : function() {
+		var that = this;
+
+		$(this.ageId).change(function() {
+			that.onAgeChange($(this));
+		});
+
+		$(this.oneAId).change(function() {
+			that.onUpperDivisionChange($(this));
+		});
+
+		$(this.openId).change(function() {
+			that.onUpperDivisionChange($(this));
+		});
+
+		$(this.ladderId).change(function() {
+			that.onLadderChange($(this));
+		});
+
+		$(this.jrId).change(function() {
+			that.onJrFreestyleChange($(this));
+		});
+		
+		$('[id^="gform_submit_button_"]').click(function() {
+			$(that.ladderId).removeAttr('disabled');
+			$(that.oneAId).removeAttr('disabled');			
+			$(that.openId).removeAttr('disabled');	
+			return true;
+		});
+	},
+
+	onLadderChange : function ( jq_element ) {
+		if ( jq_element.is(':checked') ) {
+			$(this.oneAId).removeAttr('checked').attr('disabled', 'disabled');
+			$(this.openId).removeAttr('checked').attr('disabled', 'disabled');
+		} else {
+			$(this.oneAId).removeAttr('disabled');
+			$(this.openId).removeAttr('disabled');			
+			$(this.jrId).removeAttr('checked');			
+			this.setCost(this.idLadder, this.ladderCost);
+		}
+		this.calcTotal();
+	},
+
+	onAgeChange : function ( jq_element ) {
+		if ( jq_element.val() == 0 ) {
+			$(this.oneAId).removeAttr('disabled').removeAttr('checked');
+			$(this.openId).removeAttr('disabled').removeAttr('checked');
+			$(this.ladderId).removeAttr('disabled').removeAttr('checked');
+			$(this.jrId).removeAttr('disabled');
+		} else if ( jq_element.val() < this.ageCutoff ) {
+			//$(this.ladderId).attr('checked', 'checked').attr('disabled', 'disabled');
+			$(this.oneAId).removeAttr('checked').attr('disabled', 'disabled');
+			$(this.openId).removeAttr('checked').attr('disabled', 'disabled');
+			$(this.jrId).removeAttr('disabled');
+		} else {
+			$(this.oneAId).removeAttr('disabled');
+			$(this.openId).removeAttr('disabled');
+			$(this.jrId).removeAttr('checked').attr('disabled', 'disabled');
+			//this.setCost(this.idLadder, this.ladderCost);
+			//$(this.ladderId).removeAttr('checked').removeAttr('disabled');
+		}
+		this.setCost(this.id1A, this.oneACost);
+		this.setCost(this.idOpen, this.openCost);
+		this.onLadderChange($(this.ladderId));
+		this.calcTotal();
+	},
+
+	onJrFreestyleChange : function( jq_element ) {
+		if(jq_element.is(':checked')) {
+			this.setCost(this.idLadder, '0');
+			$(this.ladderId).attr('checked', 'checked');
+		} else {
+			this.setCost(this.idLadder, this.ladderCost);
+		}
+		this.onLadderChange($(this.ladderId));
+		this.calcTotal();
+	},
+
+	//recalculates the total and also redraws price changes
+	calcTotal : function() {
+		var form_id = $('.mnyoyo-registration').attr('id');
+		gformCalculateTotalPrice(form_id.substring(6)); //just the id w/o 'gform_'
+	},
+
+	onUpperDivisionChange : function ( jq_element ) {
+		var cost1A = this.getCost(this.oneAId),
+			costOpen = this.getCost(this.openId);
+
+		if ( jq_element.attr('id') == this.idPrefix + this.id1A )
+			this.upperDivisionChange( this.id1A, this.idOpen, cost1A, costOpen, this.oneACost, this.openCost );
+		else
+			this.upperDivisionChange( this.idOpen, this.id1A, costOpen, cost1A, this.openCost, this.oneACost );
+
+		this.calcTotal();
+	},
+
+	upperDivisionChange : function ( id1, id2, cost1, cost2, origCost1, origCost2 ) {
+		var jq_id1 = this.getJqId(id1),
+			jq_id2 = this.getJqId(id2);
+
+		if ( ! $(jq_id1).is(':checked') && ! $(jq_id2).is(':checked') ) {
+			$(this.ladderId).removeAttr('disabled');
+			if ( $(this.ageId).val()  < this.ageCutoff )
+				$(this.jrId).removeAttr('disabled');
+		} else {
+			$(this.ladderId).attr('disabled', 'disabled').removeAttr('checked');
+			$(this.jrId).attr('disabled', 'disabled').removeAttr('checked');
+		}
+
+		if ( $(jq_id1).is(':checked') ) {
+			if( $(jq_id2).is(':checked') )
+				this.setCost( id1, '0' );
+			else
+				this.setCost( id2, '0' );
+		} else {
+			if( $(jq_id2).is(':checked') && cost2 != '0' ) {
+					this.setCost( id1, '0' );
+			} else {
+				if( cost1 == '0' ) {
+					this.setCost( id1, origCost2 );
+				} else {
+					this.setCost( id2, origCost1 );
+					//some reset stuff
+					if ( ! $(jq_id1).is(':checked') && ! $(jq_id2).is(':checked') )
+						this.setCost( id1, origCost1 );
+					else
+						this.setCost( id1, '0' );
+				}
+			}
+		}
+	},
+	
+	setCost : function( id_num, value ) {
+		var element = $(this.getJqId( id_num )),
+			values = element.val().split('|');
+
+		element.val(values[0] + '|' + value);
+	},
+
+	getCost : function( jq_id ) {
+		var values = $(jq_id).val().split('|');
+
+		return values[1];
+	}
+};
+
+$(document).ready(function($){ mnYoYo.init(); });
+
+})(jQuery);
